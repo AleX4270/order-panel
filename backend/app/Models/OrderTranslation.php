a8fd9bbf54bcafeddc470896721bb0dd47a9d7c0
AleX4270 <aleksr427@gmail.com>
2025-03-17 19:45:27 +0100

    feat: add the OrderTranslation model content

--- backend/app/Models/OrderTranslation.php
+++ backend/app/Models/OrderTranslation.php
@@ -5,7 +5,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class OrderTranslation extends Model
-{
-    //
+class OrderTranslation extends Model {
+    protected $table = 'order_translation';
+
+    protected $fillable = [
+        'order_id',
+        'language_id',
+        'name',
+        'remarks',
+        'description',
+    ];
 }
