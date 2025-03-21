15c76aa582923253f6c00bc088a5c98b57a051d6
AleX4270 <aleksr427@gmail.com>
2025-03-21 19:46:56 +0100

    feat: add the CountrySeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder {
     public function run(): void {
         $this->call([
             LanguageSeeder::class,
+            CountrySeeder::class,
         ]);
     }
 }
