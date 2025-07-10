2dd8aff61197d62bd989255e5f29c3418f264fa2
AleX4270 <aleksr427@gmail.com>
2025-07-10 19:05:28 +0200

    feat: add the page not found component and page not found routing wildcard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'password-recovery',
         component: PasswordRecoveryComponent,
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent,
     }
 ];
---
+++ frontend/src/app/not-found-page/not-found-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SmallFooterComponent } from "../shared/components/small-footer/small-footer.component";
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+    selector: 'app-not-found-page',
+    imports: [
+        SmallFooterComponent,
+        TranslatePipe,
+    ],
+    template: `
+        <div class="row vh-100 d-flex justify-content-center align-items-center">
+            <div class="col-3">
+                <div class="resource-not-found-card shadow-sm p-4 mt-3 bg-white">
+                    <div class="resource-not-found-card-header">
+                        <h2 class="text-danger">{{"basic.notFound" | translate}}</h2>
+                        <p class="text-muted mt-2">{{"basic.notFoundDescription" | translate}}</p>
+                    </div>
+                </div>
+                <app-small-footer/>
+            </div>
+        </div>
+    `,
+    styles: [`
+        .resource-not-found-card {
+            border: 1px solid #cce5ff;
+            border-radius: 10px;
+        }
+    `]
+})
+export class NotFoundPageComponent {}
