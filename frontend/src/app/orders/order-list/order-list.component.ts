fcd2dcabe4e19703fd806772a898647cb0d77d66
Aleks Regulski <aleks.regulski@icloud.com>
2025-09-30 19:55:56 +0200

    feat: add missing translations, add the global container padding

--- frontend/src/app/orders/order-list/order-list.component.ts
+++ frontend/src/app/orders/order-list/order-list.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-order-list',
-    imports: [],
-    template: ``,
+    imports: [
+        TranslatePipe
+    ],
+    template: `
+        <div class="row order-list-header">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-6">
+                        <h3>{{'orderList.header' | translate}}</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">Filtry</div>
+                </div>
+            </div> 
+        </div>
+
+        <div class="row order-list-info">
+            <div class="col-8">Legenda</div>
+            <div class="col-4 text-end">Przycisk dodaj zlecenie</div>
+        </div>
+
+        <div class="row order-list-table">
+            <div class="col-12">Lista</div>
+        </div>
+
+        <div class="row order-list-pagination">
+            <div class="col-12">Paginacja</div>
+        </div>
+    `,
     styles: [``]
 })
 export class OrderListComponent {
