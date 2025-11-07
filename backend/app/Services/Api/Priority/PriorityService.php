442922fc8af949b5c249e44cfb8246762e7967bc
Aleks Regulski <aleks.regulski@icloud.com>
2025-11-07 19:07:49 +0100

    feat(priorities): implement a working priority list query

--- backend/app/Services/Api/Priority/PriorityService.php
+++ backend/app/Services/Api/Priority/PriorityService.php
@@ -4,27 +4,45 @@ declare(strict_types=1);
 namespace App\Services\Api\Priority;
 
 use App\Dtos\Api\Priority\PriorityFilterDto;
+use App\Enums\SortDir;
 use App\Models\Priority;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 class PriorityService {
     public function index(PriorityFilterDto $dto): Collection {
+        // TODO: This probably can be located in a dedicated repository class
         $query = Priority::query()
             ->from('priorities as p')
             ->select([
-                'id',
-                'symbol',
-                'is_active',
+                'p.id',
+                'p.symbol',
+                'p.is_active',
+                'pt.name'
             ])
-            ->with([
-                'translations' => function($query) {
-                    $query->whereHas('language', fn($language) => $language->where('symbol', 'pl'))
-                    ->select(['name', 'priority_id']);
-                }
-            ])
-            ->active()
-            ->get();
+            ->leftJoin('priority_translations as pt', 'pt.priority_id', '=', 'p.id')
+            ->join('languages as l', function($join) {
+                $join->on('l.id', '=', 'pt.language_id')
+                    ->where('l.symbol', 'pl');
+            })
+            ->where('p.is_active', 1);
 
-        return $query;
+        match(true) {
+            default => $query->orderBy('id', SortDir::ASC->value),
+        };
+
+        $totalItems = $query->count();
+        $items = collect([]);
+
+        if(!empty($dto->page) && !empty($dto->pageSize)) {
+            $items = $query->forPage($dto->page, $dto->pageSize)->get();
+        }
+        else {
+            $items = $query->get();
+        }
+
+        return collect([
+            'items' => $items,
+            'count' => $totalItems
+        ]);
     }
 }
