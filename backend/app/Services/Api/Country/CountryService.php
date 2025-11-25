de86865f7a8e38b6d6b5f440ff8cc4af13ee6656
Aleks Regulski <aleks.regulski@icloud.com>
2025-11-25 19:54:07 +0100

    feat: add the CountryRepository

--- backend/app/Services/Api/Country/CountryService.php
+++ backend/app/Services/Api/Country/CountryService.php
@@ -4,33 +4,18 @@ declare(strict_types=1);
 namespace App\Services\Api\Country;
 
 use App\Dtos\Api\Country\CountryFilterDto;
-use App\Enums\SortDir;
-use App\Models\Country;
+use App\Repositories\CountryRepository;
 use Illuminate\Support\Collection;
 
 class CountryService {
-    public function index(CountryFilterDto $dto): Collection {
-        // TODO: This probably can be located in a dedicated repository class
-        $query = Country::query()
-            ->from('countries as c')
-            ->select([
-                'c.id',
-                'c.symbol',
-                'ct.name'
-            ])
-            ->leftJoin('country_translations as ct', 'ct.country_id', '=', 'c.id')
-            ->join('languages as l', function($join) {
-                $join->on('l.id', '=', 'ct.language_id')
-                    ->where('l.symbol', app()->getLocale());
-            });
+    public function __construct(
+        private readonly CountryRepository $countryRepository,
+    ) {}
 
-        match(true) {
-            default => $query->orderBy('id', SortDir::ASC->value),
-        };
+    public function index(CountryFilterDto $dto): Collection {
+        $query = $this->countryRepository->getAll($dto);
 
         $totalItems = $query->count();
-        $items = collect([]);
-
         if(!empty($dto->page) && !empty($dto->pageSize)) {
             $items = $query->forPage($dto->page, $dto->pageSize)->get();
         }
@@ -39,7 +24,7 @@ class CountryService {
         }
 
         return collect([
-            'items' => $items->map->toCamelCaseKeys(),
+            'items' => $items->map->toCamelCaseKeys() ?? [],
             'count' => $totalItems
         ]);
     }
