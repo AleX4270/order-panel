6553823939519535ff90ed18e644abec1f4d6085
Aleks Regulski <aleks.regulski@icloud.com>
2025-11-25 19:51:24 +0100

    feat: add the CityRepository

--- backend/app/Services/Api/City/CityService.php
+++ backend/app/Services/Api/City/CityService.php
@@ -6,36 +6,20 @@ namespace App\Services\Api\City;
 use App\Dtos\Api\City\CityDto;
 use App\Dtos\Api\City\CityFilterDto;
 use App\Dtos\Api\City\CityResolveDto;
-use App\Enums\SortDir;
 use App\Models\City;
+use App\Repositories\CityRepository;
 use Exception;
 use Illuminate\Support\Collection;
 
 class CityService {
+    public function __construct(
+        private readonly CityRepository $cityRepository,
+    ) {}
+
     public function index(CityFilterDto $dto): Collection {
-        // TODO: This probably can be located in a dedicated repository class
-        $query = City::query()
-            ->from('cities as c')
-            ->select([
-                'c.id',
-                'c.name',
-            ]);
-
-        if(!empty($dto->provinceId)) {
-            $query->where('c.province_id', $dto->provinceId);
-        }
-
-        if(!empty($dto->term)) {
-            $query->whereLike('c.name', '%'. $dto->term .'%');
-        }
-
-        match(true) {
-            default => $query->orderBy('c.id', SortDir::ASC->value),
-        };
+        $query = $this->cityRepository->getAll($dto);
 
         $totalItems = $query->count();
-        $items = collect([]);
-
         if(!empty($dto->page) && !empty($dto->pageSize)) {
             $items = $query->forPage($dto->page, $dto->pageSize)->get();
         }
@@ -44,7 +28,7 @@ class CityService {
         }
 
         return collect([
-            'items' => $items->map->toCamelCaseKeys(),
+            'items' => $items->map->toCamelCaseKeys() ?? [],
             'count' => $totalItems
         ]);
     }
