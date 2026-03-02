0f4a30168b13c882513d9d882cb8fee8c76d723e
Aleks Regulski <aleks.regulski@icloud.com>
2026-03-02 20:20:15 +0100

    fix(not-found-page): incorrect page styles and footer visibility logic

--- frontend/src/app/not-found-page/not-found-page.component.ts
+++ frontend/src/app/not-found-page/not-found-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SmallFooterComponent } from "../shared/components/small-footer/small-footer.component";
 import { TranslatePipe } from '@ngx-translate/core';
+import { Store } from '@ngxs/store';
+import { UserState } from '../shared/store/user/user.state';
 
 @Component({
     selector: 'app-not-found-page',
@@ -9,23 +11,21 @@ import { TranslatePipe } from '@ngx-translate/core';
         TranslatePipe,
     ],
     template: `
-        <div class="row vh-100 d-flex justify-content-center align-items-center">
-            <div class="col-3">
-                <div class="resource-not-found-card shadow-sm p-4 mt-3 mb-3 bg-white">
-                    <div class="resource-not-found-card-header">
-                        <h2 class="text-danger">{{"basic.notFound" | translate}}</h2>
-                        <p class="text-muted mt-2">{{"basic.notFoundDescription" | translate}}</p>
-                    </div>
+        <div class="flex flex-col justify-center items-center gap-2 mt-10">
+            <div class="card bg-base-100 max-w-lg p-2 shadow-sm">
+                <div class="card-body">
+                    <h2 class="card-title text-error">{{"basic.notFound" | translate}}</h2>
+                    <p class="text-base-content/60">{{"basic.notFoundDescription" | translate}}</p>
                 </div>
-                <app-small-footer />
             </div>
+            @if(!isAuthenticated()) {
+                <app-small-footer />
+            }
         </div>
     `,
-    styles: [`
-        .resource-not-found-card {
-            border: 1px solid var(--resource-not-found-card-background-color);
-            border-radius: var(--radius-lg);
-        }
-    `]
+    styles: [``]
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+    private readonly store = inject(Store);
+    protected readonly isAuthenticated = this.store.selectSignal(UserState.isAuthenticated);
+}
