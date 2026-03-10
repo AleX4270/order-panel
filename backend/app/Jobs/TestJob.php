e07fc9271f163b3873f0225cc331c03190f06fe4
Aleks Regulski <aleks.regulski@icloud.com>
2026-03-10 19:51:11 +0100

    feat(docker-compose): add the backend queue service

---
+++ backend/app/Jobs/TestJob.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class TestJob implements ShouldQueue {
+    use Queueable;
+
+    public function __construct() {}
+
+    public function handle(): void {
+        logger('The queue is working!');
+    }
+}
