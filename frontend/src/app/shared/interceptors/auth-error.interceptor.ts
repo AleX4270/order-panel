b1f6dcecca17f6aeb31ff983737fc8319a32fc12
Aleks Regulski <aleks.regulski@icloud.com>
2026-05-05 21:05:35 +0200

    fix: user not logged out after changing its own password

--- frontend/src/app/shared/interceptors/auth-error.interceptor.ts
+++ frontend/src/app/shared/interceptors/auth-error.interceptor.ts
@@ -1,18 +1,30 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
-import { HttpStatus } from '../enums/enums';
+import { catchError, switchMap, tap, throwError } from 'rxjs';
+import { HttpStatus, ToastType } from '../enums/enums';
 import { inject } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { LogoutUser } from '../store/user/user.actions';
+import { Router } from '@angular/router';
+import { ToastService } from '../services/toast/toast.service';
+import { TranslateService } from '@ngx-translate/core';
 
 export const authErrorInterceptor: HttpInterceptorFn = (req, next) => {
     const store = inject(Store);
+    const router = inject(Router);
+    const toastService = inject(ToastService);
+    const translateService = inject(TranslateService);
 
     return next(req).pipe(
         catchError((error: HttpErrorResponse) => {
             if(error.status === HttpStatus.UNAUTHORIZED || error.status === HttpStatus.PAGE_EXPIRED) {
-                store.dispatch(new LogoutUser());
-                return throwError(() => error);
+                return store.dispatch(new LogoutUser())
+                    .pipe(
+                        tap(() => {
+                            toastService.show(translateService.instant('basic.sessionExpired'), ToastType.warning);
+                            return router.navigate(['/']);
+                        }),
+                        switchMap(() => throwError(() => error))
+                    );
             }
 
             return throwError(() => error);
