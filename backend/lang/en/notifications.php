fd598c389d4d2c506a1fc53fbe2d40a77f72aaf6
Aleks Regulski <aleks.regulski@icloud.com>
2026-05-24 19:55:01 +0200

    feat(backend): add missing english translations

--- backend/lang/en/notifications.php
+++ backend/lang/en/notifications.php
@@ -4,6 +4,12 @@ return [
     //Order completed
     'orderCompletedTitle' => 'Order completed',
     'orderCompletedMessage' => 'Order #:orderId has been marked as completed.',
+
+    //Order incoming deadline
     'orderIncomingDeadlineTitle' => 'Order deadline approaching',
     'orderIncomingDeadlineMessage' => 'The deadline for order #:orderId is :date',
+
+    //Order request created
+    'orderRequestCreatedTitle' => 'New order request',
+    'orderRequestCreatedMessage' => 'A new order request with number #:orderRequestId has been received',
 ];
