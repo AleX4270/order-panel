e2ecee295e7c55854015b74fa3a3330b6e0fb683
Aleks Regulski <aleks.regulski@icloud.com>
2026-08-18 14:00:55 +0200

    chore: add missing strict_types declarations

--- backend/app/Exceptions/Api/OrderStatus/OrderStatusNotFoundException.php
+++ backend/app/Exceptions/Api/OrderStatus/OrderStatusNotFoundException.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Exceptions\Api\OrderStatus;
 
 use Illuminate\Http\Request;
--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use App\Console\Commands\SendIncomingDeadlineNotifications;
 use Illuminate\Support\Facades\Schedule;
