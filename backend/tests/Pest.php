a07981eb495eee9fa07d376f9549676ba79b07d5
Alex4270 <aleks.regulski@icloud.com>
2026-08-29 19:26:43 +0200

    feat(pest): add the ability to pass permissions to actingAsApiUser function

--- backend/tests/Pest.php
+++ backend/tests/Pest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Enums\PermissionType;
 use App\Models\User;
 use Tests\TestCase;
 
@@ -9,9 +10,15 @@ pest()->extend(Tests\TestCase::class)
     ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
-function actingAsApiUser(): TestCase {
+function actingAsApiUser(PermissionType ...$permissions): TestCase {
+    $user = User::factory()->create();
+
+    if(!empty($permissions)) {
+        $user->syncPermissions(array_map(fn($permission) => $permission->value, $permissions));
+    }
+
     return test()
-        ->actingAs(User::factory()->create(), 'web')
+        ->actingAs($user, 'web')
         ->withHeaders([
             'Origin' => 'http://localhost'
         ]);
